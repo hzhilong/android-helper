4b56ff31e9f909d6314fdb9634b50c4ff853eb82
hzhilong <1107495265@qq.com>
2024-12-31 19:45:31 +0800

    -

--- config/index.js
+++ config/index.js
@@ -1,29 +1,18 @@
 const devEnv = require("./env.dev")
 const prodEnv = require("./env.prod")
 
-// 全局配置参数
-// 名称
-let APP_NAME = "Android 助手"
-// 是否开发模式 也可以自己读process.env吧
-let IS_DEV_MODE = true
-let APP_AUTHOR = "hzhilong"
-let GITHUB_URL = "https://github.com/hzhilong/androidhelper"
-
-module.exports = {
-  APP_NAME,
-  IS_DEV_MODE,
-  APP_AUTHOR,
-  GITHUB_URL,
-  setParams(key, value) {
-    module.exports[key] = value
-  },
+const config = {
+  APP_NAME: process.env.VUE_APP_PRODUCT_NAME,
+  IS_DEV_MODE: process.env.NODE_ENV !== "production",
+  APP_AUTHOR: process.env.VUE_APP_AUTHOR_NAME,
+  GITHUB_URL: process.env.VUE_APP_AUTHOR_URL,
 }
 
-// 初始化
-module.exports.IS_DEV_MODE = process.env.NODE_ENV !== "production"
-let configParams = module.exports.IS_DEV_MODE ? devEnv : prodEnv
+let configParams = config.IS_DEV_MODE ? devEnv : prodEnv
 Object.keys(configParams).forEach(key => {
-  if (module.exports[key] !== undefined) {
-    module.exports[key] = configParams[key]
+  if (config[key] !== undefined) {
+    config[key] = configParams[key]
   }
 })
+
+module.exports = config
--- vue.config.js
+++ vue.config.js
@@ -1,46 +1,24 @@
 "use strict"
-const Config = require("./config/index")
 const aliasConfig = require("./alias.config")
 const webpack = require("webpack")
+const packageJson = require("./package.json")
+const path = require("path")
 
-process.env.VUE_APP_NAME = require("./package.json").name
-process.env.VUE_APP_VERSION = require("./package.json").version
+process.env.VUE_APP_NAME = packageJson.name
+process.env.VUE_APP_PRODUCT_NAME = packageJson.productName
+process.env.VUE_APP_VERSION = packageJson.version
+process.env.VUE_APP_AUTHOR_NAME = packageJson.author.name
+process.env.VUE_APP_AUTHOR_URL = packageJson.author.url
 
 module.exports = function resolveClientEnv(options, raw) {
-  let envParams = {}
-  Object.keys(process.env).forEach(key => {
-    if (key.startsWith("VUE_APP_")) {
-      envParams[key] = process.env[key]
-    }
-  })
-  if (Object.keys(envParams).length > 0) {
-    console.log("-------------环境变量-------------")
-    Object.keys(envParams).forEach(key => {
-      console.log(key, "\t", envParams[key])
-    })
-  }
-  console.log("-------------配置参数-------------")
-  console.log(Config)
-  console.log("---------------------------------")
-
   return {
     devServer: {
       port: 8087,
-      proxy: {
-        "/apkInfo": {
-          target: "https://***apk/", // 你请求的第三方接口
-          changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-          pathRewrite: {
-            // 路径重写，
-            "^/apkInfo/": "", // 替换target中的请求地址，也就是说以后你在请求http://api.douban.com/v2/XXXXX这个地址的时候直接写成/api即可。
-          },
-        },
-      },
     },
     publicPath: "./",
     chainWebpack: config => {
       config.plugin("html").tap(args => {
-        args[0].title = Config.APP_NAME + " " + process.env.VUE_APP_VERSION
+        args[0].title = process.env.VUE_APP_PRODUCT_NAME + " " + process.env.VUE_APP_VERSION
         return args
       })
       let aliasKeys = Object.keys(aliasConfig.resolve.alias)
@@ -66,14 +44,22 @@ module.exports = function resolveClientEnv(options, raw) {
           win: {
             icon: "./public/app_logo.ico",
           },
-          mac: {
-            // icon: "./public/app.png",
-          },
-          productName: process.env.VUE_APP_NAME,
-          asar: false,
+          mac: {},
+          productName: process.env.VUE_APP_PRODUCT_NAME,
+          artifactName: process.env.VUE_APP_NAME + "_" + process.env.VUE_APP_VERSION + ".${ext}",
+          asar: true,
           extraResources: {
             from: "./resources/adb/",
             to: "./adb",
+          },
+          nsis: {
+            oneClick: false,
+            perMachine: false,
+            allowToChangeInstallationDirectory: true,
+            deleteAppDataOnUninstall: false,
+            license: path.join(__dirname, "LICENSE"),
+            createDesktopShortcut: true,
+            createStartMenuShortcut: true,
           },
         },
       },
